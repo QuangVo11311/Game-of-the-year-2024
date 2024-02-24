0a5beaebd834c9cad9077d015dc87ef791a84b4d
Võ Đăng Quang <94774447+QuangVo11311@users.noreply.github.com>
2024-02-24 10:20:47 +0700

    update camera group

--- code/level.py
+++ code/level.py
@@ -5,33 +5,51 @@ from player import Player
 from debug import debug
 
 class Level:
-    def __init__(self):
-        
-        # get the díplay surface
-        self.display_surface = pygame.display.get_surface()
+	def __init__(self):
 
-        # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
-        self.obstacle_sprites = pygame.sprite.Group()
+		# get the display surface 
+		self.display_surface = pygame.display.get_surface()
 
-        # sprite setup
-        self.create_map()
+		# sprite group setup
+		self.visible_sprites = YSortCameraGroup()
+		self.obstacle_sprites = pygame.sprite.Group()
 
-    def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'P':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+		# sprite setup
+		self.create_map()
 
-            # print(row_index)
-            # print(row)
+	def create_map(self):
+		for row_index, row in enumerate(WORLD_MAP):
+			for col_index, col in enumerate(row):
+				x = col_index * TILESIZE
+				y = row_index * TILESIZE
+				if col == 'x':
+					Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
+				if col == 'P':
+					self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
 
-    def run(self):
-        # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
-        self.visible_sprites.update()
-        debug(self.player.direction)
+	def run(self):
+		# update and draw the game
+		self.visible_sprites.custom_draw(self.player)
+		self.visible_sprites.update()
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+	def __init__(self):
+
+		# general setup 
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
+
+	def custom_draw(self,player):
+
+		# getting the offset 
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
+
+		# for sprite in self.sprites():
+		for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image, offset_pos)
