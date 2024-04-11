89f10a3728fb959ae013973e52dfd87aadec158e
Võ Đăng Quang <94774447+QuangVo11311@users.noreply.github.com>
2024-04-11 15:04:26 +0700

    thay đổi sức mạnh
    tất cả quái
    máu tăng 50
    tấn công tăng 5
    nhân vật tăng chỉ số máu và mana
    máu: 200 -> 300
    mana: 140 -> 210

--- code/settings.py
+++ code/settings.py
@@ -11,9 +11,9 @@ HITBOX_OFFSET = {
 }
 
 # ui 
-BAR_HEIGHT = 20
-HEALTH_BAR_WIDTH = 200
-ENERGY_BAR_WIDTH = 140
+BAR_HEIGHT = 25
+HEALTH_BAR_WIDTH = 300
+ENERGY_BAR_WIDTH = 210
 ITEM_BOX_SIZE = 80
 UI_FONT = '../graphics/font/joystix.ttf'
 UI_FONT_SIZE = 18
@@ -52,8 +52,8 @@ magic_data = {
 
 # enemy
 monster_data = {
-	'squid': {'health': 100, 'exp': 100, 'damage': 20, 'attack_type': 'slash', 'attack_sound': '../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
-	'raccoon': {'health': 300, 'exp': 250, 'damage': 40, 'attack_type': 'claw', 'attack_sound': '../audio/attack/claw.wav', 'speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
-	'spirit': {'health': 100, 'exp': 110, 'damage': 8, 'attack_type': 'thunder', 'attack_sound': '../audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
-	'bamboo': {'health': 70, 'exp': 120, 'damage': 6, 'attack_type': 'leaf_attack', 'attack_sound': '../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}
+	'squid': {'health': 150, 'exp': 100, 'damage': 25, 'attack_type': 'slash', 'attack_sound': '../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
+	'raccoon': {'health': 350, 'exp': 250, 'damage': 45, 'attack_type': 'claw', 'attack_sound': '../audio/attack/claw.wav', 'speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
+	'spirit': {'health': 150, 'exp': 110, 'damage': 13, 'attack_type': 'thunder', 'attack_sound': '../audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
+	'bamboo': {'health': 120, 'exp': 120, 'damage': 11, 'attack_type': 'leaf_attack', 'attack_sound': '../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}
 }
